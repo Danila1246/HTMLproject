3482c1a77cb8786a992b7624cdaed16b0bf9dfe6
Danila1246 <48722452+Danila1246@users.noreply.github.com>
2023-07-12 10:04:45 +0700

    Initial commit

--- htmlprojectversionalpha001/app_advert/views.py
+++ htmlprojectversionalpha001/app_advert/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(requests):
     return render(requests, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
