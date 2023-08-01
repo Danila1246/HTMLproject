db97f511a9f58f6f049bfa7ab3967bd24b1cdb6c
Danila1246 <48722452+Danila1246@users.noreply.github.com>
2023-08-01 15:12:43 +0700

    Initial commit

--- htmlprojectversionalpha001/app_advert/models.py
+++ htmlprojectversionalpha001/app_advert/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Advertisment(models.Model):
+    title = models.CharField(verbose_name='заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    auction = models.BooleanField('торг', help_text='Отметьте, если уместен торг')
+
 # Create your models here.
