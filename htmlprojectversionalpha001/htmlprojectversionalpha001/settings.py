af5f4ff528c7e13d7077b7c7fcada2bd0a2910ac
Danila1246 <48722452+Danila1246@users.noreply.github.com>
2023-08-01 15:19:14 +0700

    Initial commit

--- htmlprojectversionalpha001/htmlprojectversionalpha001/settings.py
+++ htmlprojectversionalpha001/htmlprojectversionalpha001/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
     }
 }
 
+#ENGINE - движок
+#service - файл службы подключения
+#passfile - файл паролей
+#OPTIONS - опции
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
