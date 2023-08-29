371e911ab0e80213d7a710746a8fb131c76f248e
Danila1246 <48722452+Danila1246@users.noreply.github.com>
2023-08-29 18:26:03 +0700

    Initial commit

--- htmlprojectversionalpha001/app_advert/views.py
+++ htmlprojectversionalpha001/app_advert/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render, reverse, redirect
 from .models import Advertisment
 from .forms import AdvertisementForm
-# Create your views here.
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+
 
 
 def index(request):
     advertisements = Advertisment.objects.all()
     context = {'advertisements': advertisements}
-    return render(request, 'index.html', context)
+    return render(request, 'app_advert/index.html', context)
 
 
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advert/top-sellers.html')
 
+@login_required(login_url=reverse_lazy('login'))
 def advertisement_post(request):
     if request.method == 'POST':
         form = AdvertisementForm(request.POST, request.FILES)
         if form.is_valid():
-            advertisement = Advertisment(**form.cleaned_data)
+            advertisement = form.save(commit=False)
             advertisement.user = request.user
             advertisement.save()
             return redirect(reverse('main-page'))
     form = AdvertisementForm()
     context = {'form': form}
-    return render(request, 'advertisement-post.html', context)
+    return render(request, 'app_advert/advertisement-post.html', context)
